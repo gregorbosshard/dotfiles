fce79f5544fda17d7f57ee9766d8a04611dee746
Gregor Bosshard <gbo@gbosshard.ch>
2024-10-21 11:25:21 +0200

    update brew install

--- brew/brew.zsh
+++ brew/brew.zsh
@@ -9,12 +9,13 @@ brew install starship
 brew install node
 brew install dotnet
 brew install neovim
+brew install python
 
 brew install --cask google-chrome
 brew install --cask 1password
 brew install --cask alacritty
+brew install --cask visual-studio-code
 
 brew install --cask font-jetbrains-mono-nerd-font
 
 brew cleanup
-
